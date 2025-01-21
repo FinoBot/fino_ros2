691b37d0e515c4c5fcadea5e9535d8ff8a63ee5e
Alexis Carella <carellaalexis@gmail.com>
2025-01-21 17:11:43 +0100

    fix: pull and start script

--- pull_and_start.sh
+++ pull_and_start.sh
@@ -8,4 +8,5 @@ cd ../fino_ros2_msgs
 git pull
 cd ..
 docker build -f fino_ros2/Dockerfile -t finoros2 .
-docker run --name finobot_ros2 --restart unless-stopped --privileged --network host -v /dev/ttyS0:/dev/ttyS0 -v /dev/bus/usb:/dev/bus/usb finoros2 /bin/bash -c "source install/setup.bash && ros2 launch fino_ros2 finobot_launch.py"
+docker run -d --name finobot_ros2 --restart unless-stopped --privileged --network host -v /dev/ttyS0:/dev/ttyS0 -v /dev/bus/usb:/dev/bus/usb finoros2 /bin/bash -c "source install/setup.bash && ros2 launch fino_ros2 finobot_launch.py"
+docker logs -f finobot_ros2
