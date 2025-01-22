c30d740c8597a2fca3c18103811ab6bd1a563d75
Alexis Carella <carellaalexis@gmail.com>
2025-01-22 14:11:36 +0100

    revert to vosk

--- fino_ros2/audio_recognition.py
+++ fino_ros2/audio_recognition.py
@@ -3,47 +3,58 @@ import rclpy
 from rclpy.node import Node
 import sounddevice as sd
 import numpy as np
-import speech_recognition as sr
+from vosk import Model, KaldiRecognizer
 from std_msgs.msg import String
+import json
 import threading
-
-wordlist = ["suis-moi", "stop", "debout", "coucou", "salut", "hey"]
 
 class AudioRecognition(Node):
     def __init__(self):
         super().__init__('audio_recognition')
         self.get_logger().info(f'Current working directory: {os.getcwd()}')
-        
-        self.recognizer = sr.Recognizer()
+        model_path = '/root/ros2_ws/src/fino_ros2/model/vosk-model-small-fr-0.22'
+        self.model = Model(model_path)
+        self.recognizer = KaldiRecognizer(self.model, 48000)
+    
         self.audio_commands = self.create_publisher(String, '/audio_commands', 10)
 
         self.get_logger().info('Audio Recognition Node has been started.')
         self.audio_queue = []
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+    # def audio_callback(self, indata, frames, time, status):
+    #     if status:
+    #         self.get_logger().error(str(status))
+        
+    #     audio_data = np.frombuffer(indata, dtype=np.int16)
+    #     self.audio_queue.append(audio_data)
 
     def audio_callback(self, indata, frames, time, status):
         if status:
             self.get_logger().error(str(status))
         
         audio_data = np.frombuffer(indata, dtype=np.int16)
-        self.audio_queue.append(audio_data)
+        
+        # Apply gain to amplify the audio
+        gain = 3.0  # Adjust the gain factor as needed
+        amplified_audio = np.clip(audio_data * gain, -32768, 32767).astype(np.int16)
+        
+        self.audio_queue.append(amplified_audio)
+
 
     def process_audio(self):
         while rclpy.ok():
             if self.audio_queue:
                 audio_data = self.audio_queue.pop(0)
-                audio_data = audio_data.tobytes()
-                audio = sr.AudioData(audio_data, 48000, 2)
-                try:
-                    result = self.recognizer.recognize_sphinx(audio, keyword_entries=[(word, 1.0) for word in wordlist])
-                    self.get_logger().info(result)
-                    if result != "":
+                if self.recognizer.AcceptWaveform(audio_data.tobytes()):
+                    result = json.loads(self.recognizer.Result())
+                    self.get_logger().info(result["text"])
+                    if result["text"] != "":
                         msg = String()
-                        msg.data = result
+                        msg.data = result["text"]
                         self.audio_commands.publish(msg)
-                except sr.UnknownValueError:
-                    self.get_logger().info("Sphinx could not understand audio")
-                except sr.RequestError as e:
-                    self.get_logger().error(f"Sphinx error; {e}")
 
     def start_listening(self):
         threading.Thread(target=self.process_audio, daemon=True).start()
