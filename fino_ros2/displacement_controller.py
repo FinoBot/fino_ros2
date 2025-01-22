adcf5742a5526a4817e90343be6403a8c11266ed
Alexis Carella <carellaalexis@gmail.com>
2025-01-22 18:14:56 +0100

    fix: adjusted lost person delay

--- fino_ros2/displacement_controller.py
+++ fino_ros2/displacement_controller.py
@@ -46,7 +46,7 @@ class DisplacementController(Node):
             self.send_command('kbalance')
 
     def handle_person_lost(self):
-        lost_person_following_delay = 3
+        lost_person_following_delay = 1.5
         lost_person_delay = 25
 
         if self.lost_person_ts is None:
